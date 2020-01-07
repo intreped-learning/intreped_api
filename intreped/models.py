6bd46b84a358e16970b96379af768021f58506ef
Tyler Schaffer <tpschaffer@gmail.com>
2020-01-07 08:40:43 -0700

    Make vital fields unique in db models

--- intreped/models.py
+++ intreped/models.py
@@ -2,23 +2,23 @@ from django.db import models
 
 
 class Course(models.Model):
-    title = models.CharField(max_length=100)
+    title = models.CharField(max_length=100, unique=True)
     description = models.CharField(max_length=1000)
-    url = models.CharField(max_length=300)
+    url = models.CharField(max_length=300, unique=True)
     category = models.CharField(max_length=50)
-    thumbnail = models.CharField(max_length=300)
+    thumbnail = models.CharField(max_length=300, unique=True)
     duration = models.CharField(max_length=20)
 
 
 class Badge(models.Model):
-    category = models.CharField(max_length=50)
+    category = models.CharField(max_length=50, unique=True)
 
 
 class Teacher(models.Model):
     name = models.CharField(max_length=50)
-    username = models.CharField(max_length=20)
+    username = models.CharField(max_length=20, unique=True)
     password = models.CharField(max_length=20)
-    email = models.CharField(max_length=50)
+    email = models.CharField(max_length=50, unique=True)
     courses = models.ManyToManyField(Course, through='TeacherCourse')
     badges = models.ManyToManyField(Badge, through='TeacherBadge')
 
@@ -31,8 +31,14 @@ class TeacherCourse(models.Model):
     is_complete = models.BooleanField(default=False)
     is_in_progress = models.BooleanField(default=False)
 
+    class Meta:
+        unique_together = ('teacher_id', 'course_id',)
+
 
 class TeacherBadge(models.Model):
     teacher_id = models.ForeignKey(Teacher, on_delete=models.CASCADE)
     badge_id = models.ForeignKey(Badge, on_delete=models.CASCADE)
     cumulative_time = models.CharField(max_length=10)
+
+    class Meta:
+        unique_together = ('teacher_id', 'badge_id',)
